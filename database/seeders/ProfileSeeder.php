0c5395e4565eaa3c3ea7708a4f397c8c6460cc9d
AR Shahin <mdshahinmije96@gmail.com>
2021-07-20 12:00:02 +0600

    seeder and tinker

---
+++ database/seeders/ProfileSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Profile;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+
+class ProfileSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            Profile::create([
+                'user_id' => $i,
+                'country' => ucwords(Str::random(5)),
+                'city' => ucwords(Str::random(5)),
+                'post_code' => $i % 2 == 0 ? rand(100, 5000) : null
+            ]);
+        }
+    }
+}
